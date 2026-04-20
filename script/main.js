3268b7e9d9fba9680ff945f126c3853a9853b6b7
titoValen <valentin.tito@davinci.edu.ar>
2026-04-20 17:45:06 -0300

    Add API endpoint constants for characters, episodes, families, and locations

---
+++ script/main.js
@@ -0,0 +1,4 @@
+const URL_CHARACTERS = "https://spapi.dev/api/characters/";
+const URL_EPISODES = "https://spapi.dev/api/episodes/";
+const URL_FAMILIES = "https://spapi.dev/api/families/";
+const URL_LOCATIONS = "https://spapi.dev/api/locations/";

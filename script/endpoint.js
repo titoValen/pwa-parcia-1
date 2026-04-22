c63ab1586e9a8220a237b55b0fe208cdf978bbb9
titoValen <valentin.tito@davinci.edu.ar>
2026-04-22 13:11:24 -0300

    Actualizar URLs en endpoint.js: corregir enlaces a la API de Los Simpson y eliminar referencia a familias

--- script/endpoint.js
+++ script/endpoint.js
@@ -1,11 +1,9 @@
-const URL_CHARACTERS = "https://spapi.dev/api/characters/";
-const URL_EPISODES = "https://spapi.dev/api/episodes/";
-const URL_FAMILIES = "https://spapi.dev/api/families/";
-const URL_LOCATIONS = "https://spapi.dev/api/locations/";
+const URL_CHARACTERS = "https://thesimpsonsapi.com/api/characters";
+const URL_EPISODES = "https://thesimpsonsapi.com/api/episodes";
+const URL_LOCATIONS = "https://thesimpsonsapi.com/api/locations";
 
 export const endpoint = {
   characters: URL_CHARACTERS,
   episodes: URL_EPISODES,
-  families: URL_FAMILIES,
   locations: URL_LOCATIONS,
 };

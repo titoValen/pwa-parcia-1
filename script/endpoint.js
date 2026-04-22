a0be12d644ccc4050dc5483e8eec2e0dbbcb549b
titoValen <valentin.tito@davinci.edu.ar>
2026-04-21 22:45:44 -0300

    Reorganizar constantes de API: mover a endpoint.js y eliminar main.js

---
+++ script/endpoint.js
@@ -0,0 +1,11 @@
+const URL_CHARACTERS = "https://spapi.dev/api/characters/";
+const URL_EPISODES = "https://spapi.dev/api/episodes/";
+const URL_FAMILIES = "https://spapi.dev/api/families/";
+const URL_LOCATIONS = "https://spapi.dev/api/locations/";
+
+export const endpoint = {
+  characters: URL_CHARACTERS,
+  episodes: URL_EPISODES,
+  families: URL_FAMILIES,
+  locations: URL_LOCATIONS,
+};
--- script/main.js
+++
@@ -1,4 +0,0 @@
-const URL_CHARACTERS = "https://spapi.dev/api/characters/";
-const URL_EPISODES = "https://spapi.dev/api/episodes/";
-const URL_FAMILIES = "https://spapi.dev/api/families/";
-const URL_LOCATIONS = "https://spapi.dev/api/locations/";

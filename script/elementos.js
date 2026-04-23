c209edc42a098da909c23f5392a57fbbf6b15ea5
titoValen <valentin.tito@davinci.edu.ar>
2026-04-23 13:58:11 -0300

    Agregar constante imageCDN en elementos.js para almacenar la URL de la API de imágenes

--- script/elementos.js
+++ script/elementos.js
@@ -1,3 +1,4 @@
 export const $ = document;
 export const renderizar = $.getElementById("renderizar");
 export const li = $.querySelectorAll("li");
+export const imageCDN = "https://cdn.thesimpsonsapi.com/200";

fbd9159306625d66c3ad98bec29145a78b0fdde6
titoValen <valentin.tito@davinci.edu.ar>
2026-04-23 13:58:21 -0300

    Importar imageCDN en functions.js y habilitar renderización de episodios y ubicaciones

--- script/functions.js
+++ script/functions.js
@@ -1,5 +1,5 @@
 // functions.js
-import { $ } from "./elementos.js";
+import { $, imageCDN } from "./elementos.js";
 
 export function renderizarData(id, datos, contenedor) {
   switch (id) {
@@ -8,11 +8,11 @@ export function renderizarData(id, datos, contenedor) {
       break;
 
     case "episodes":
-      // renderizarEpisodes(datos, contenedor);
+      renderizarEpisodes(datos, contenedor);
       break;
 
     case "locations":
-      // renderizarLocations(datos, contenedor);
+      renderizarLocations(datos, contenedor);
       break;
     default:
       console.error(`ID no reconocido: ${id}`);
@@ -25,8 +25,46 @@ function renderizarCharacters(datos, contenedor) {
     const card = $.createElement("div");
     card.classList.add("card");
     card.innerHTML = `
+      <figure>
+        <img src="${imageCDN}${datos.results[i].portrait_path}" alt="${datos.results[i].name}">
+      </figure>  
       <h2>${datos.results[i].name}</h2>
       <p>${datos.results[i].occupation}</p>
+    `;
+    contenedor.appendChild(card);
+  }
+}
+
+function renderizarEpisodes(datos, contenedor) {
+  for (let i = 0; i < datos.results.length; i++) {
+    const card = $.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+      <figure>
+        <img src="${imageCDN}${datos.results[i].image_path}" alt="${datos.results[i].name}">
+      </figure>  
+    <h2>${datos.results[i].name}</h2>
+      <p>${datos.results[i].synopsis}</p>
+      <div>
+        <span>Temporada: ${datos.results[i].season}</span>
+        <span>Episodio: ${datos.results[i].episode_number}</span>
+        <span>Fecha de emisión: ${datos.results[i].airdate}</span>
+      </div>
+    `;
+    contenedor.appendChild(card);
+  }
+}
+
+function renderizarLocations(datos, contenedor) {
+  for (let i = 0; i < datos.results.length; i++) {
+    const card = $.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+      <figure>
+        <img src="${imageCDN}${datos.results[i].image_path}" alt="${datos.results[i].name}">
+      </figure>  
+      <h2>${datos.results[i].name}</h2>
+      <p>${datos.results[i].town}</p>
     `;
     contenedor.appendChild(card);
   }

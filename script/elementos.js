f1aeb28219c18c7cf8e42ad568512087737b9989
titoValen <valentin.tito@davinci.edu.ar>
2026-04-24 01:04:42 -0300

    Agregar exportaciones para elementos de navegación en elementos.js

--- script/elementos.js
+++ script/elementos.js
@@ -2,3 +2,6 @@ export const $ = document;
 export const renderizar = $.getElementById("renderizar");
 export const li = $.querySelectorAll("li");
 export const imageCDN = "https://cdn.thesimpsonsapi.com/200";
+export const containerBtn = $.querySelector(".container-btn");
+export const btnAnterior = $.querySelector(".btn-ant");
+export const btnSiguiente = $.querySelector(".btn-sig");
